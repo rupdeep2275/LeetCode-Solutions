6752c48ca02b2e2acafb1c47bd2dfc985139d68c
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2023-11-03 02:51:02 +0530

    Update 1458-sort-integers-by-the-number-of-1-bits.py

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.py
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.py
@@ -14,27 +14,5 @@ class Solution:
             res[i] = res[i] % 10001
         return res
 
-----------------------------------------------------------------------------
 
-class Solution {
-    public int[] sortByBits(int[] arr) {
-        int n = arr.length;
-        int[] res = new int[n];
-        for(int i=0; i<n; i++){
-            res[i] = setBits(arr[i])*10001 + arr[i];
-        }
-        Arrays.sort(res);
-        for(int i=0; i<n; i++){
-            res[i] = res[i] % 10001;
-        }
-        return res;
-    }
-    public int setBits(int n){
-        int count = 0;
-        while(n > 0){
-            count += (n & 1);
-            n = n >> 1;
-        }
-        return count;
-    }
-}
+
