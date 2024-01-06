69ebb5053f3459e1e79498464aad48f3aeb29863
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2024-01-06 23:59:13 +0530

    Create 392. Is Subsequence.py

---
+++ 392. Is Subsequence.py
@@ -0,0 +1,33 @@
+#Python:
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+        if i == len(s):
+            return True
+        return False
+
+#Java:
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        if(i == s.length()) return true;
+        return false;
+    }
+}
