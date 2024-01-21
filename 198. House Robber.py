aab5c0ca917b38e7e9f6253b56261d8abc7821f2
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2024-01-21 13:34:46 +0530

    Create 198. House Robber.py

---
+++ 198. House Robber.py
@@ -0,0 +1,28 @@
+#Python:
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        dp = [0] * n
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[n-1]
+
+#Java:
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(var i=2; i<n; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[n-1];
+    }
+}
