b6e9c588326338408096caabe1543b203acae098
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2024-02-13 13:14:44 +0530

    Create 2108. Find First Palindromic String in the Array.py

---
+++ 2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,34 @@
+#Python:
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        def is_palindrome(s):
+            i, j = 0, len(s) - 1
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        for word in words:
+            if is_palindrome(word):
+                return word
+        return ""
+      
+#Java:
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        def is_palindrome(s):
+            i, j = 0, len(s) - 1
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        for word in words:
+            if is_palindrome(word):
+                return word
+        return ""
+
