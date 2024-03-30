527ab40665c30297031b720081c7fe74c8f77f93
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2024-03-30 22:25:55 +0530

    Update 76. Minimum Window Substring.java

--- 76. Minimum Window Substring.java
+++ 76. Minimum Window Substring.java
@@ -1,12 +1,12 @@
 class Solution {
     public String minWindow(String s, String t) {
         var map = new HashMap<Character, Integer>();
+        var window = new HashMap<Character, Integer>();
         for(char c : t.toCharArray()){
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        var window = new HashMap<Character, Integer>();
-        int i = 0, minLen = s.length() + 1, index = -1, required = map.size(), formed = 0;
-        for(int j = 0; j < s.length(); j++){
+        int i = 0, index = -1, minLen = s.length() + 1, required = map.size(), formed = 0;
+        for(var j=0; j<s.length(); j++){
             char c = s.charAt(j);
             window.put(c, window.getOrDefault(c, 0) + 1);
             if(map.containsKey(c) && window.get(c).intValue() == map.get(c).intValue()){
@@ -15,8 +15,8 @@ class Solution {
             while(i <= j && formed == required){
                 c = s.charAt(i);
                 if(minLen > j - i + 1){
-                    index = i;
                     minLen = j - i + 1;
+                    index = i;
                 }
                 window.put(c, window.get(c) - 1);
                 if(map.containsKey(c) && window.get(c).intValue() < map.get(c).intValue()){
