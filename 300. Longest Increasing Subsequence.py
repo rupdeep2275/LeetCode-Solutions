736d524a8e08199893d9c950b6be7dd9ac661490
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-02-10 01:29:11 +0530

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -16,19 +16,19 @@ class Solution:
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        for(int i=1; i<nums.length; i++){
+        int n = nums.length;
+        int[] dp = new int[n];
+        for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
                 if(nums[i] > nums[j]){
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
-        int res = 1;
+        int res = 0;
         for(int num : dp){
-            res = Math.max(num, res);
+            res = Math.max(res, num);
         }
-        return res;
+        return res + 1;
     }
 }
