1786ed4232ee6ca69c6d76b9e54abb569456d3c3
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-02-13 21:24:23 +0530

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,12 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         var map = new HashMap<Integer, Integer>();
-        for(var i=0; i<nums.length; i++){
-            if(map.containsKey(target-nums[i])){
-                return new int[]{map.get(target-nums[i]), i};
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
             }
             map.put(nums[i], i);
         }
-        return new int[]{0,0};
+        return new int[]{};
     }
 }
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = dict()
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in map:
+                return [map[comp], i]
+            map[nums[i]] = i
+        return []
