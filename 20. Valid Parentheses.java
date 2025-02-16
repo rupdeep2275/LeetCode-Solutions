770cee6419f74a31da12c6a17352b44eb0709d72
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-02-17 00:57:19 +0530

    Update 20. Valid Parentheses.java

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
         var stack = new Stack<Character>();
-        for(var i=0; i<s.length(); i++){
+        for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(!stack.isEmpty()){
-                if((c == ')' && stack.peek() == '(') || 
-                    (c == '}' && stack.peek() == '{') || 
-                    (c == ']' && stack.peek() == '[')){
-                        stack.pop();
-                        continue;
-                }
-            }
-            stack.push(c);
+            if(c == '(') stack.push(')');
+            else if(c == '[') stack.push(']');
+            else if(c == '{') stack.push('}');
+            else if(stack.isEmpty() || stack.pop() != c) return false;
         }
         return stack.isEmpty();
     }
