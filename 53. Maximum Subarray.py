de83a0806ae9e0a2e0d2f240e69ece43d1015b32
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-02-20 01:18:43 +0530

    Update and rename 53. Maximum Subarray.java to 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,3 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res, sum = float('-inf'), 0
+        for num in nums:
+            sum += num
+            res = max(sum, res)
+            if sum < 0:
+                sum = 0
+        return res
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum = 0, res = Integer.MIN_VALUE;
