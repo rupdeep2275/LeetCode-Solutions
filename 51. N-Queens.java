ec314ba0ad77bcf95d3a2f2b1819818e5c8818a1
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-02-23 01:31:28 +0530

    Update 51. N-Queens.java

--- 51. N-Queens.java
+++ 51. N-Queens.java
@@ -1,31 +1,28 @@
 class Solution {
     int n;
-    List<List<String>> res;
+    char[][] state;
+    Set<Integer> cols = new HashSet<>(), dias = new HashSet<>(), antiDias = new HashSet<>();
+    List<List<String>> res = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
         this.n = n;
-        res = new ArrayList<>();
-        var emptyBoard = new char[n][n];
-        Arrays.stream(emptyBoard).forEach(row -> Arrays.fill(row, '.'));
-        backtrack(0, new HashSet<>(), new HashSet<>(), new HashSet<>(), emptyBoard);
+        state = new char[n][n];
+        Arrays.stream(state).forEach(row -> Arrays.fill(row, '.'));
+        backtrack(0);
         return res;
     }
-    void backtrack(int row, Set<Integer> cols, Set<Integer> dias, Set<Integer> antiDias, char[][] state){
+    void backtrack(int row){
         if(row == n){
             res.add(Arrays.stream(state).map(String::new).toList());
             return;
         }
-        for(var col=0; col<n; col++){
-            var dia = row - col;
-            var antiDia = row + col;
+        for(int col=0; col<n; col++){
+            int dia = row - col, antiDia = row + col;
             if(cols.contains(col) || dias.contains(dia) || antiDias.contains(antiDia)) continue;
-
-            cols.add(col); dias.add(dia); antiDias.add(antiDia);
             state[row][col] = 'Q';
-
-            backtrack(row+1,cols, dias, antiDias, state);
-
-            cols.remove(col); dias.remove(dia); antiDias.remove(antiDia);
+            cols.add(col); dias.add(dia); antiDias.add(antiDia);
+            backtrack(row+1);
             state[row][col] = '.';
+            cols.remove(col); dias.remove(dia); antiDias.remove(antiDia);
         }
     }
 }
