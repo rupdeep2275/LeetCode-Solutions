df45465cc2ecb58c8705cbc86b9b9461ee330b0e
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-02-25 00:51:15 +0530

    Update 189. Rotate Array.java

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -1,18 +1,16 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        var n = nums.length;
+        int n = nums.length;
         k %= n;
-        reverse(nums, 0, n-1);
+        reverse(nums, 0, n - 1);
         reverse(nums, 0, k-1);
-        reverse(nums, k, n-1);
+        reverse(nums, k, n - 1);
     }
     void reverse(int[] nums, int i, int j){
         while(i < j){
-            var temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+            int temp = nums[i];
+            nums[i++] = nums[j];
+            nums[j--] = temp;
         }
     }
 }
