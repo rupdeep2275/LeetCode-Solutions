7a89f05d75956d9e0bdf01e7fba9a7ac30313f8d
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-03-16 21:51:02 +0530

    Update 912. Sort an Array.java

--- 912. Sort an Array.java
+++ 912. Sort an Array.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        var n = nums.length;
-        mergeSort(nums, 0, n-1);
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
-    void mergeSort(int[] arr, int s, int e){
-        if(s == e) return;
-        int m = (s+e)/2;
-        mergeSort(arr, s, m);
-        mergeSort(arr, m+1, e);
-        merge(arr, s, m, e);
+    void mergeSort(int[] nums, int lo, int hi){
+        if(lo == hi) return;
+        int mid = (lo + hi) >> 1;
+        mergeSort(nums, lo, mid);
+        mergeSort(nums, mid + 1, hi);
+        merge(nums, lo, mid, hi);
     }
-    void merge(int[] arr, int s, int m, int e){
-        int i = s, j = m+1, x = 0;
-        var temp = new int[e-s+1];
-        while(i <= m && j <= e){
-            if(arr[i] < arr[j]){
-                temp[x++] = arr[i++];
-            }
-            else temp[x++] = arr[j++];
+    void merge(int[] nums, int lo, int mid, int hi){
+        int i = lo, j = mid + 1, x = 0;
+        int[] temp = new int[hi - lo + 1];
+        while(i <= mid && j <= hi){
+            if(nums[i] < nums[j]) temp[x++] = nums[i++];
+            else temp[x++] = nums[j++];
         }
-        while(i <= m) temp[x++] = arr[i++];
-        while(j <= e) temp[x++] = arr[j++];
-        System.arraycopy(temp, 0, arr, s, temp.length);
+        while(i <= mid) temp[x++] = nums[i++];
+        while(j <= hi) temp[x++] = nums[j++];
+        System.arraycopy(temp, 0, nums, lo, temp.length);
     }
 }
