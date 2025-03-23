80a5bd94d7075e5af7675ea0c95655589c3c7ea6
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-03-24 00:57:23 +0530

    Create 138. Copy List with Random Pointer.java

---
+++ 138. Copy List with Random Pointer.java
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> map = new HashMap<>();
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        if(map.get(head) != null) return map.get(head);
+
+        Node node = new Node(head.val);
+        map.put(head, node);
+        node.next = copyRandomList(head.next);
+        node.random = copyRandomList(head.random);
+        return node;
+    }
+}
