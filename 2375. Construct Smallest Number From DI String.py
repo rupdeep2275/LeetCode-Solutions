34a32f06e1c5bdf79da08d81559ab052eede9614
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-03-31 22:58:04 +0530

    Update and rename 2375. Construct Smallest Number From DI String.java to 2375. Construct Smallest Number From DI String.py

--- 2375. Construct Smallest Number From DI String.py
+++ 2375. Construct Smallest Number From DI String.py
@@ -1,3 +1,16 @@
+#Python:
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        s = []
+        stack = []
+        for i in range(len(pattern) + 1):
+            stack.append(str(i+1))
+            if i == len(pattern) or pattern[i] == 'I':
+                while stack:
+                    s.append(stack.pop())
+        return ''.join(s)
+
+#Java:
 class Solution {
     public String smallestNumber(String pattern) {
         Stack<Integer> stack = new Stack<>();
