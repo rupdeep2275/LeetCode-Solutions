ec9b4d6c6422ef1f8157992c077347823aeb7347
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-04-01 12:56:23 +0530

    Update 42. Trapping Rain Water.py

--- 42. Trapping Rain Water.py
+++ 42. Trapping Rain Water.py
@@ -2,6 +2,22 @@
 
 class Solution:
     def trap(self, height: List[int]) -> int:
+        lo, hi, l_max, r_max, water = 0, len(height) - 1 , 0, 0, 0
+        while lo < hi:
+            if height[lo] < height[hi]:
+                l_max = max(l_max, height[lo])
+                water += l_max - height[lo]
+                lo += 1
+            else:
+                r_max = max(r_max, height[hi])
+                water += r_max - height[hi]
+                hi -= 1
+        return water
+
+#intuitive:
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
         n, max_till_now, res = len(height), 0 , 0
         max_l = [0] * n
         max_r = [0] * n
@@ -34,5 +50,27 @@ class Solution {
             }
         }
         return water;
+    }
+}
+
+#intuitive:
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length, maxTillNow = height[0], res = 0;;
+        int[] maxL = new int[n], maxR = new int[n];
+        for(var i=1; i<n; i++){
+            maxL[i] = maxTillNow;
+            maxTillNow = Math.max(maxTillNow, height[i]);
+        }
+        maxTillNow = height[n-1];
+        for(var i=n-2; i>=0; i--){
+            maxR[i] = maxTillNow;
+            maxTillNow = Math.max(maxTillNow, height[i]);
+        }
+        for(var i=1; i<n-1; i++){
+            res += Math.max(0, Math.min(maxL[i], maxR[i]) - height[i]);
+        }
+        return res;
     }
 }
