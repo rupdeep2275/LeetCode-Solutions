bb2a9a4791b9927516e577aee5647b28f5cd56da
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-04-09 23:38:00 +0530

    Update and rename 1642. Furthest Building You Can Reach.java to 1642. Furthest Building You Can Reach.py

--- 1642. Furthest Building You Can Reach.py
+++ 1642. Furthest Building You Can Reach.py
@@ -1,21 +1,20 @@
-// Java:
+#Java:
 
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        var pq = new PriorityQueue<Integer>();
-        for(var i=0; i<heights.length-1; i++){
-            var d = heights[i+1] - heights[i];
-            if(d > 0) pq.add(d);
-            if(pq.size() > ladders){
-                bricks -= pq.poll();
-            }
-            if(bricks < 0) return i;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=1; i<heights.length; i++){
+            int diff = heights[i] - heights[i-1];
+            if(diff <= 0) continue;
+            pq.add(diff);
+            if(pq.size() > ladders) bricks -= pq.poll();
+            if(bricks < 0) return i-1;
         }
         return heights.length - 1;
     }
 }
 
-//Python:
+#Python:
 
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
