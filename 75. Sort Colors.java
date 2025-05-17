e43fc66e5f382110465f599a2044ca1265d44285
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-05-17 14:39:27 +0530

    Update 75. Sort Colors.java

--- 75. Sort Colors.java
+++ 75. Sort Colors.java
@@ -1,15 +1,14 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int red = 0, white = 0, blue = 0;
-        for(var n : nums){
-            if(n == 0) red++;
-            else if(n == 1) white++;
-            else blue++;
+        int zeros = 0, ones = 0;
+        for(int num : nums){
+            if(num == 0) zeros++;
+            else if(num == 1) ones++;
         }
-        for(var i=0; i<nums.length; i++){
-            if(i < red) nums[i] = 0;
-            else if(i < red + white) nums[i] = 1;
-            else nums[i] = 2;
+        for(int i=0; i<nums.length; i++){
+            if(i < zeros) nums[i] = 0;
+            else if(i < zeros + ones) nums[i] = 1;
+            else nums[i]= 2;
         }
     }
 }
