d23c77d39603c3a6940c96a5507fe4ae01ae2c7e
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2025-06-11 20:37:01 +0530

    Update 205. Isomorphic Strings.java

--- 205. Isomorphic Strings.java
+++ 205. Isomorphic Strings.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        var map1 = new HashMap<Character, Integer>();
-        var map2 = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> map1 = new HashMap<>(), map2 = new HashMap<>();
         for(Integer i=0; i<s.length(); i++){
             if(map1.put(s.charAt(i), i) != map2.put(t.charAt(i), i)) return false;
         }
