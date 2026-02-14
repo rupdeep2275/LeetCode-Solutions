e0542b6747677dd2f72a22ed462ec09ecb0e0a03
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2026-02-14 17:32:03 +0530

    Create 799. Champagne Tower.java

---
+++ 799. Champagne Tower.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] tower = new double[101][101];
+        tower[0][0] = (double) poured;
+        for(int i=0; i<=query_row; i++){
+            for(int j=0; j<=i; j++){
+                double q = (tower[i][j] - 1)/2.0;
+                if(q > 0){
+                    tower[i+1][j] += q;
+                    tower[i+1][j+1] += q;
+                }
+            }
+        }
+        return Math.min(1, tower[query_row][query_glass]);
+    }
+}
